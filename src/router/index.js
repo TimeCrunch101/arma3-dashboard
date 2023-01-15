497d78721bdca4e5f26e397ae651bec4a47cb019
Aaron Allen <91556430+TimeCrunch101@users.noreply.github.com>
2023-01-15 17:59:44 -0500

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-// import axios from "axios"
+import axios from "axios"
 
 
 const router = createRouter({
@@ -9,14 +9,17 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: () => import('../views/HomeView.vue'),
-      // beforeEnter: () => {
-      //   axios.get("/query/first-time-setup").then((res) => {
-      //     const response = res.data
-      //     if (response.config === false && response.error === false) {
-      //       router.push("/first-time-setup")
-      //     }
-      //   })
-      // }
+      beforeEnter: () => {
+        axios.get("/query/first-time-setup").then((res) => {
+          console.log(res.data)
+          // const response = res.data
+          // if (response.config === false && response.error === false) {
+          //   router.push("/first-time-setup")
+          // }
+        }).catch((err) => {
+          console.log(err.response.data)
+        })
+      }
     },
     {
       path: '/first-time-setup',
