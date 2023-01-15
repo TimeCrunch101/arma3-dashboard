c0a4d3c032e68755090ff9763b644bc1e396b5e1
Aaron Allen <91556430+TimeCrunch101@users.noreply.github.com>
2023-01-14 23:08:03 -0500

    properly log out error response

--- src/components/ConfigForm.vue
+++ src/components/ConfigForm.vue
@@ -31,7 +31,7 @@ const updateConfig = () => {
         shouldDefinePassword: form.value.shouldDefinePassword,
         userPassword: form.value.userPassword
     }).catch((error) => {
-        console.error(error)
+        console.error(error.response.data)
     })
 }
 
