5cc47ee31e64964a42da44cd9352bfd2b93a0c81
Aaron Allen <91556430+TimeCrunch101@users.noreply.github.com>
2023-01-18 18:51:41 -0500

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -11,11 +11,9 @@ const router = createRouter({
       component: () => import('../views/HomeView.vue'),
       beforeEnter: () => {
         axios.get("/query/first-time-setup").then((res) => {
-          console.log(res.data)
-          // const response = res.data
-          // if (response.config === false && response.error === false) {
-          //   router.push("/first-time-setup")
-          // }
+          if (res.data.serverStatus === false && res.data.error === null) {
+            router.push("/first-time-setup")
+          }
         }).catch((err) => {
           console.log(err.response.data)
         })
